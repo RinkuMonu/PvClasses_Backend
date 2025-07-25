efe3248c72bf2211958aeb961ce0b502f52400b0
RinkuMonu <rinkuyadav9460@gmail.com>
2025-07-25 11:20:10 +0530

    Add GitHub Actions workflow for deploying Node.js backend and update server port to 5006

--- server.js
+++ server.js
@@ -119,7 +119,7 @@ mongoose
     console.log("Connected to MongoDB");
 
     // Start server
-    const PORT = process.env.PORT || 5000;
+    const PORT = process.env.PORT || 5006;
     app.listen(PORT, () => {
       console.log(
         `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`
